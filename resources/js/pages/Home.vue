9239bc2af97c30bb1d90e018455fd0b2881dbd58
Tamrat Assefa Kessito <mamushassefa@gmail.com>
2020-03-18 19:36:02 +0300

    add something to display on the homepage

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,3 +1,30 @@
 <template>
-  <h2 class="py-6 text-base">Home Page - Work In Progress</h2>
+  <div>
+    <nfl-container></nfl-container>
+    <ncaaf-container></ncaaf-container>
+    <nba-container></nba-container>
+    <ncaab-container></ncaab-container>
+    <mlb-container></mlb-container>
+    <nhl-container></nhl-container>
+  </div>
 </template>
+
+<script>
+import NBA from './../containers/NBA';
+import NFL from './../containers/NFL';
+import NHL from './../containers/NHL';
+import MLB from './../containers/MLB';
+import NCAAB from './../containers/NCAAB';
+import NCAAF from './../containers/NCAAF';
+
+export default {
+  components: {
+    'nba-container': NBA,
+    'nfl-container': NFL,
+    'nhl-container': NHL,
+    'mlb-container': MLB,
+    'ncaaf-container': NCAAF,
+    'ncaab-container': NCAAB,
+  }
+}
+</script>
