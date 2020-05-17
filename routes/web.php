9dba4f83442f83b73e4ce0d473fb67cb89ac42bb
Tamrat Assefa Kessito <mamushassefa@gmail.com>
2020-05-17 10:59:30 +0300

    modify routes so that non existent api routes will return a 404

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@
 Route::post('/login', 'Auth\LoginController@login');
 Route::post('/logout', 'Auth\LoginController@logout');
 Route::post('/register', 'Auth\RegisterController@register');
-Route::get('{any}', 'FrontEndController')->where('any', '.*');
+// Route::get('{any}', 'FrontEndController')->where('any', '.*');
+Route::get('{any}', 'FrontEndController')->where('any', '^(?!api).*$');
