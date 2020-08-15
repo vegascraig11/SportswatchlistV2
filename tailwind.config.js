d387c6fc0c013608675a6f724526e3fc626000b0
Tamrat Assefa Kessito <mamushassefa@gmail.com>
2020-08-15 14:39:03 +0300

    setup purging of unused css classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,30 +5,35 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ["Arial", ...defaultTheme.fontFamily.sans],
-        inter: ["Inter", ...defaultTheme.fontFamily.sans]
+        inter: ["Inter", ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        'mantis': {
-          100: '#F1F9EE',
-          200: '#DDF0D5',
-          300: '#C8E6BC',
-          400: '#9ED489',
-          500: '#75C157',
-          600: '#69AE4E',
-          700: '#467434',
-          800: '#355727',
-          900: '#233A1A',
+        mantis: {
+          100: "#F1F9EE",
+          200: "#DDF0D5",
+          300: "#C8E6BC",
+          400: "#9ED489",
+          500: "#75C157",
+          600: "#69AE4E",
+          700: "#467434",
+          800: "#355727",
+          900: "#233A1A",
         },
         "swl-black": {
           lighter: "#27282c",
           light: "#1c1e21",
-          dark: "#0b0b0b"
-        }
-      }
-    }
+          dark: "#0b0b0b",
+        },
+      },
+    },
   },
   variants: {
-    visibility: ['group-hover']
+    visibility: ["group-hover"],
   },
-  plugins: []
+  plugins: [],
+  purge: [
+    "./resources/js/pages/**/*.vue",
+    "./resources/js/components/**/*.vue",
+    "./resources/js/containers/**/*.vue",
+  ],
 };
