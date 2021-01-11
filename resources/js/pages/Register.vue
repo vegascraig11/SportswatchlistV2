54eaf0a4bc0257e8f5cdee2e88369a9ecdb020d0
Tamart Assefa Kessito <mamushassefa@gmail.com>
2021-01-12 00:33:49 +0300

    show error messages on registration

--- resources/js/pages/Register.vue
+++ resources/js/pages/Register.vue
@@ -5,37 +5,105 @@
     <form class="max-w-sm" @submit.prevent="register">
       <div class="mt-2">
         <label for="firstname">First Name</label>
-        <div><input class="w-full px-2 py-1 border rounded" :class="errors.firstname ? 'border-red-500' : ''" type="text" v-model="firstname" id="firstname" required placeholder="John"></div>
-        <p v-if="errors.firstname" class="text-red-700">{{ errors.firstname }}</p>
+        <div>
+          <input
+            class="w-full px-2 py-1 border rounded"
+            :class="errors.firstname ? 'border-red-500' : ''"
+            type="text"
+            v-model="firstname"
+            id="firstname"
+            required
+            placeholder="John"
+          />
+        </div>
+        <p v-if="errors.firstname" class="text-red-700">
+          {{ errors.firstname[0] }}
+        </p>
       </div>
       <div class="mt-2">
         <label for="lastname">Last Name</label>
-        <div><input class="w-full px-2 py-1 border rounded" :class="errors.lastname ? 'border-red-500' : ''" type="text" v-model="lastname" id="lastname" required placeholder="Doe"></div>
-        <p v-if="errors.lastname" class="text-red-700">{{ errors.lastname }}</p>
+        <div>
+          <input
+            class="w-full px-2 py-1 border rounded"
+            :class="errors.lastname ? 'border-red-500' : ''"
+            type="text"
+            v-model="lastname"
+            id="lastname"
+            required
+            placeholder="Doe"
+          />
+        </div>
+        <p v-if="errors.lastname" class="text-red-700">
+          {{ errors.lastname[0] }}
+        </p>
       </div>
       <div class="mt-2">
         <label for="email">Email</label>
-        <div><input class="w-full px-2 py-1 border rounded" :class="errors.email ? 'border-red-500' : ''" type="email" v-model="email" id="email" required placeholder="john@example.com"></div>
-        <p v-if="errors.email" class="text-red-700">{{ errors.email }}</p>
+        <div>
+          <input
+            class="w-full px-2 py-1 border rounded"
+            :class="errors.email ? 'border-red-500' : ''"
+            type="email"
+            v-model="email"
+            id="email"
+            required
+            placeholder="john@example.com"
+          />
+        </div>
+        <p v-if="errors.email" class="text-red-700">{{ errors.email[0] }}</p>
       </div>
       <div class="mt-2">
         <label for="password">Password</label>
-        <div><input class="w-full px-2 py-1 border rounded" :class="errors.password ? 'border-red-500' : ''" type="password" v-model="password" id="password" required placeholder="Password"></div>
-        <p v-if="errors.password" class="text-red-700">{{ errors.password }}</p>
+        <div>
+          <input
+            class="w-full px-2 py-1 border rounded"
+            :class="errors.password ? 'border-red-500' : ''"
+            type="password"
+            v-model="password"
+            id="password"
+            required
+            placeholder="Password"
+          />
+        </div>
+        <p v-if="errors.password" class="text-red-700">
+          {{ errors.password[0] }}
+        </p>
       </div>
       <div class="mt-2">
         <label for="confirmPassword">Confirm Password</label>
-        <div><input class="w-full px-2 py-1 border rounded" :class="errors.confirmPassword ? 'border-red-500' : ''" type="password" v-model="confirmPassword" id="confirmPassword" required placeholder="Confirm Password"></div>
-        <p v-if="errors.confirmPassword" class="text-red-700">{{ errors.confirmPassword }}</p>
+        <div>
+          <input
+            class="w-full px-2 py-1 border rounded"
+            :class="errors.confirmPassword ? 'border-red-500' : ''"
+            type="password"
+            v-model="confirmPassword"
+            id="confirmPassword"
+            required
+            placeholder="Confirm Password"
+          />
+        </div>
+        <p v-if="errors.confirmPassword" class="text-red-700">
+          {{ errors.confirmPassword[0] }}
+        </p>
       </div>
       <div class="mt-4">
         <label for="agreement" class="flex align-center">
-          <input v-model="agreement" type="checkbox" id="agreement">
-          <span class="ml-2" :class="errors.agreement ? 'text-red-500' : ''">I agree to the <a class="underline text-gray-700 cursor-pointer" href="#">terms and conditions</a> of Sports Watchlist.</span>
+          <input v-model="agreement" type="checkbox" id="agreement" />
+          <span class="ml-2" :class="errors.agreement ? 'text-red-500' : ''"
+            >I agree to the
+            <a class="underline text-gray-700 cursor-pointer" href="#"
+              >terms and conditions</a
+            >
+            of Sports Watchlist.</span
+          >
         </label>
       </div>
       <div class="mt-4">
-        <button class="bg-mantis-500 text-white px-4 py-2 font-semibold rounded hover:bg-green-500">Sign Up</button>
+        <button
+          class="bg-mantis-500 text-white px-4 py-2 font-semibold rounded hover:bg-green-500"
+        >
+          Sign Up
+        </button>
       </div>
     </form>
   </div>
@@ -45,64 +113,84 @@
 export default {
   data() {
     return {
-      firstname: '',
-      lastname: '',
-      email: '',
-      password: '',
-      confirmPassword: '',
-      agreement: '',
-      errors: {}
-    }
+      firstname: "",
+      lastname: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      agreement: "",
+      errors: {},
+    };
   },
   methods: {
     register() {
-      const { firstname, lastname, email, password, confirmPassword, agreement } = this;
+      const {
+        firstname,
+        lastname,
+        email,
+        password,
+        confirmPassword,
+        agreement,
+      } = this;
 
       if (!this.validateFields()) return;
 
-      console.log('registering user...')
-      axios.post('/register', {
-        name: `${firstname} ${lastname}`,
-        email,
-        password,
-        password_confirmation: confirmPassword
-      })
-        .then(response => {
-          this.$store.commit('registerMessage', 'Your user account was created.');
-          this.$router.push('/login');
+      axios
+        .post("/register", {
+          name: `${firstname} ${lastname}`,
+          email,
+          password,
+          password_confirmation: confirmPassword,
         })
-        .catch(err => console.log(err));
+        .then(response => {
+          this.$success(
+            "Success",
+            "Your account has been created successfully. You can now login."
+          );
+          this.$router.push("/login");
+        })
+        .catch(err => {
+          this.$error("Register Error", err.response.data.message);
+          this.errors = err.response.data.errors;
+        });
     },
     validateFields() {
-      const { firstname, lastname, email, password, confirmPassword, agreement } = this;
-      const errors = {}
+      const {
+        firstname,
+        lastname,
+        email,
+        password,
+        confirmPassword,
+        agreement,
+      } = this;
+      const errors = {};
 
-      if (firstname.trim() === '')
-        errors['firstname'] = 'First name is required.';
+      if (firstname.trim() === "")
+        errors["firstname"] = ["First name is required."];
 
-      if (lastname.trim() === '')
-        errors['lastname'] = 'Last name is required.';
+      if (lastname.trim() === "")
+        errors["lastname"] = ["Last name is required."];
 
-      if (email.trim() === '')
-        errors['email'] = 'Email is required.';
+      if (email.trim() === "") errors["email"] = ["Email is required."];
 
-      if (password === '')
-        errors['password'] = 'Password is required.';
+      if (password === "") errors["password"] = ["Password is required."];
 
       if (password.length < 8)
-        errors['password'] = 'Password needs to be at least 8 characters.';
+        errors["password"] = ["Password needs to be at least 8 characters."];
 
       if (password !== confirmPassword)
-        errors['confirmPassword'] = 'The passwords do not match.';
+        errors["confirmPassword"] = ["The passwords do not match."];
 
       if (!agreement) {
-        errors['agreement'] = 'You have to agree to the terms and conditions.';
+        errors["agreement"] = [
+          "You have to agree to the terms and conditions.",
+        ];
       }
 
-      this.errors = errors
+      this.errors = errors;
 
       return Object.keys(this.errors).length === 0;
-    }
-  }
-}
+    },
+  },
+};
 </script>
